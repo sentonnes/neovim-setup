573e4b3a4635af1b3c37a67e723af3bf2e82fa19
Scott James <scott.james@ukho.gov.uk>
2026-07-22 10:27:36 +0100

    Add centre scroll for navigation

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Allow overrides
 vim.o.exrc = true
 vim.o.secure = true -- still requires manual trust confirmation per file, safer default
 
+-- Use the PowerShell 7 commandline
 vim.o.shell = "pwsh"
 vim.o.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command"
+
+-- Set the scroll to be centre of the screen when moving up and down
+vim.o.scrolloff = 999
